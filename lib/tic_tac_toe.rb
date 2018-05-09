2af7550fadac9d91ec1b76f3b2b8c93051bb1b6e
Alex Wilson <fonzmp@gmail.com>
2018-05-09 00:12:48 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -41,4 +41,16 @@ class TicTacToe
     end
   end
 
+  def valid_move?(index)
+    if index.between?(0, 8)
+      if board[index] == " " || board[index] == "" || board[index] == nil
+        return true
+      else
+        return false
+      end
+    else
+      return false
+    end
+  end
+
 end
