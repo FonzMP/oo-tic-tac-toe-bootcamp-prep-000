f704c9a35c97b3faee31c1a384306fa74927483e
Alex Wilson <fonzmp@gmail.com>
2018-05-09 00:29:31 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -100,7 +100,7 @@ class TicTacToe
   end
 
   def won?
-    board_test = board.all?{|letter| letter == "" or letter == " "}
+    board_test = @board.all?{|letter| letter == "" or letter == " "}
     if board_test
       return false
     else
@@ -109,9 +109,9 @@ class TicTacToe
         win_index_2 = win_combination[1]
         win_index_3 = win_combination[2]
 
-        position_1 = board[win_index_1]
-        position_2 = board[win_index_2]
-        position_3 = board[win_index_3]
+        position_1 = @board[win_index_1]
+        position_2 = @board[win_index_2]
+        position_3 = @board[win_index_3]
 
         if position_1 == "X" && position_2 == "X" && position_3 == "X"
           return win_combination
