cf31b76e30849c1f14d548b11636351d8ae7d315
Alex Wilson <fonzmp@gmail.com>
2018-05-09 00:16:25 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -72,19 +72,19 @@ class TicTacToe
   def turn
     puts "Please enter 1-9:"
     user_input = gets.to_i
-    user_index = input_to_index(user_input)
-    if user_index.between?(0, 8)
-      if valid_move?(@board, user_index)
-        @board[user_index] = current_player(@board)
-        display_board(@board)
-      else
-        puts "Sorry, that spot is taken!"
-        turn(@board)
-      end
-    else
-      puts "That is not a valid choice. Please try again:"
-      turn(@board)
-    end
+    # user_index = input_to_index(user_input)
+    # if user_index.between?(0, 8)
+    #   if valid_move?(@board, user_index)
+    #     @board[user_index] = current_player(@board)
+    #     display_board(@board)
+    #   else
+    #     puts "Sorry, that spot is taken!"
+    #     turn(@board)
+    #   end
+    # else
+    #   puts "That is not a valid choice. Please try again:"
+    #   turn(@board)
+    # end
   end
 
 end
