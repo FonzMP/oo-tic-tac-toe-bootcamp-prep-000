911a291a3569bc271c9b81e99089cf606b481c51
Alex Wilson <fonzmp@gmail.com>
2018-05-09 00:32:28 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -175,7 +175,7 @@ class TicTacToe
 
     if winner == "X"
       puts "Congratulations X!"
-    elsif winner(board) == "O"
+    elsif winner == "O"
       puts "Congratulations O!"
     else
       puts "Cat's Game!"
