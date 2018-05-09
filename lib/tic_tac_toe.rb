80f9b5d8b2cde4bb91ea64f0583cf1b0e165e90a
Alex Wilson <fonzmp@gmail.com>
2018-05-09 00:14:37 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -65,12 +65,8 @@ class TicTacToe
     return count
   end
 
-  def current_player(board)
-    if turn_count(board) % 2 == 0
-      return "X"
-    else
-      return "O"
-    end
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
   end
 
 end
